f35d0878ea19e43fc5734f210596def9827cc383
Johan <jaas.alexander01@gmail.com>
2025-12-21 17:00:00 -0500

    Creacion de clase Producto

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -6,6 +6,17 @@ from datetime import datetime
 class EntityBase(BaseModel):
     id: str
     fecha_Creacion: datetime = Field(default_factory=datetime.now)
-    autor: str = "Equipo de desarroll"
+    autor: str = "Equipo de Desarrollo"
+
+class Producto(EntityBase):
+    nombre: str
+    codigoBarras: str
+    precio: float
+    categoria: str
+    stock: int
+    active: bool = True
+
+
+
 
 
