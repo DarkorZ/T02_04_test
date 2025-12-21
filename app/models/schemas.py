6b6ab281c2797889690d55ccdd0490df99deb23d
Johan <jaas.alexander01@gmail.com>
2025-12-21 16:55:30 -0500

    Creación de  clase BaseModel

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,1 +1,11 @@
 #Creacion del esquema del modelo
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from datetime import datetime
+
+class EntityBase(BaseModel):
+    id: str
+    fecha_Creacion: datetime = Field(default_factory=datetime.now)
+    autor: str = "Equipo de desarroll"
+
+
