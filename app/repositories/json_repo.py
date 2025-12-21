3230657047c55e3553d87c98efa023cadcb584a2
John Serrano <jcristofer13@gmail.com>
2025-12-21 12:36:22 -0500

    Avance del desarrollo de los repositorios

--- app/repositories/json_repo.py
+++ app/repositories/json_repo.py
@@ -1,1 +1,14 @@
 #Creacion del repositorio JSON
+
+import json
+import os
+from typing import List, Dict
+class JSONRepository:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        folder = os.path.dirname(self.file_path)
+        if folder and not os.path.exists(folder):
+            os.makedirs(folder)
+        if not os.path.exists(self.file_path):
+            with open(self.file_path, 'w') as f:
+                json.dump([], f)
