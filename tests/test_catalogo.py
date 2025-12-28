026aa0db18698e1cb19cd07b553bfc88057aee89
josephTC2003 <jatc33fb@gmail.com>
2025-12-27 20:00:54 -0500

    Agregar una solicitud POST para una prueba de solicitud exitosa

--- tests/test_catalogo.py
+++ tests/test_catalogo.py
@@ -3,3 +3,14 @@ def test_crear_producto_exitoso(client):
     # Se ajustan los datos enviados al endpoint para que coincidan
     # exactamente con el esquema Pydantic definido en schemas.py.
     # Esto evita errores de validación (422 Unprocessable Entity).
+        response = client.post(
+        "/catalogo/crear",
+        json={
+            "id": "P001",
+            "nombre": "Arroz",
+            "codigoBarras": "1234567890",
+            "precio": 2.5,
+            "categoria": "Granos",
+            "stock": 10
+        }
+    )
